cf480afa414c579e5bbfd92ab56cb47e284e50b1
Daria Novikova <novikovadarig@yandex.ru>
2024-11-15 01:47:39 +0300

    Three commits after rebase

--- app1/src/main/java/org/example/Main.java
+++ app1/src/main/java/org/example/Main.java
@@ -5,5 +5,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello everybody!");
         System.out.println("Goodbye everybody!");
+        System.out.println("First!");
+        System.out.println("Second!");
+        System.out.println("Third!");
     }
 }

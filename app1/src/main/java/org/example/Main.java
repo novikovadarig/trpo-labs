6994aa4881f81f4f41d5e2a257ee8e551cb2b5fe
Daria Novikova <novikovadarig@yandex.ru>
2024-12-12 16:55:14 +0300

    Создание класса animal в dev3

--- app1/src/main/java/org/example/Main.java
+++ app1/src/main/java/org/example/Main.java
@@ -8,5 +8,21 @@ public class Main {
         System.out.println("First!");
         System.out.println("Second!");
         System.out.println("Third!");
+
+        // 5 пункт - Слияние веток
+        Animal animalByPerson1 = new Animal("person1");
+        System.out.println("Animal with name: " + animalByPerson1.getName());
+    }
+
+    public static class Animal {
+        private final String name;
+
+        public Animal(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }

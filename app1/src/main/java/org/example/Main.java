46f74db3cf62f2e3841f6c5ecb886f7f0868a277
Daria Novikova <novikovadarig@yandex.ru>
2024-12-12 17:07:43 +0300

    Редактирование Main в dev

--- app1/src/main/java/org/example/Main.java
+++ app1/src/main/java/org/example/Main.java
@@ -2,11 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello everybody!");
-        System.out.println("Goodbye everybody!");
-        System.out.println("First!");
-        System.out.println("Second!");
-        System.out.println("Third!");
+        System.out.println("Нам не нужно столько лишних выводов в консоли!");
     }
 }

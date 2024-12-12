9a3cb18c1a36a88b1e0e36c508a7cc38cea7f534
Daria Novikova <novikovadarig@yandex.ru>
2024-12-12 17:02:45 +0300

    Создание класса car в dev2

--- app1/src/main/java/org/example/App2.java
+++ app1/src/main/java/org/example/App2.java
@@ -3,5 +3,21 @@ package org.example;
 public class App2 {
     public static void main(String[] args) {
         System.out.println("Test amend commit");
+
+        // 5 пункт - Слияние веток
+        Car carByPerson2 = new Car("Audi for person2");
+        System.out.println("Car with name: " + carByPerson2.getName());
+    }
+
+    public static class Car {
+        private String name;
+
+        public Car(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
